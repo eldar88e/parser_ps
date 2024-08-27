14989ebd6e6ff7b8e5330cf532b42da2c32c864b
Eldar Eminov <em88el@gmail.com>
2024-08-28 02:01:21 +0300

    Update GoodJob settings

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,8 @@ module RailsParser
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.active_job.queue_adapter = :good_job
+
     # Don't generate system test files.
     config.generators.system_tests = nil
 
